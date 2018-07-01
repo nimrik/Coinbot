fb860516ee0ea01c9dfdee46f8ceceb71bd8619c
nimrik <nimrik87@gmail.com>
2018-07-02 00:06:43 +0300

    first push with 3 pages

---
+++ scripts/events.js
@@ -0,0 +1,28 @@
+var dropdown = document.querySelector(".hamburger-box");
+
+dropdown.addEventListener("click", function() {
+  dropdown.classList.toggle("is-active");
+});
+
+var dropdownArray = document.getElementsByClassName("c-nav__header");
+var dropdownItem = document.getElementsByClassName("c-nav__header-item");
+
+for(let i = 0; i < dropdownItem.length; i++) {
+  dropdownItem[i].addEventListener("click", function(e) {
+    e.target.classList.toggle("is-active");
+  });
+}
+
+var questions = document.getElementsByClassName("p-questions");
+
+for(let i = 0; i < questions.length; i++) {
+  questions[i].addEventListener("click", function() {
+    questions[i].classList.toggle("is-active");
+  });
+}
+
+var checkbox = document.querySelector(".o-label--checkbox");
+
+checkbox.addEventListener("click", function() {
+  checkbox.classList.toggle("is-active");
+});
